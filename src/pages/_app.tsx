9cecdb7520b7c645bede979ce71e3280ceec20f0
Matsuoka Kensuke <quantumular@gmail.com>
2022-06-04 23:58:46 +0900

    feat: render MDX

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../../styles/globals.css";
+import "prism-themes/themes/prism-dracula.css";
+
+import { MDXProvider } from "@mdx-js/react";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider>
+      <div className="prose mx-auto max-w-3xl">
+        <Component {...pageProps} />
+      </div>
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
